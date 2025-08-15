dc274fb8b3cd6638228d4013c201b97888ebb0f6
Kuba <kba1337@wp.pl>
2025-08-15 18:50:00 +0200

    final ver

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import triton
 import triton.testing
 from dot_kernel import dot
 
-SIZE = 1024 // 2
+SIZE = 1024
 torch.manual_seed(0)
 DEVICE = torch.device('cuda')
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
         x_names=['size'],  # Argument names to use as an x-axis for the plot.
-        x_vals=[2**i for i in range(SIZE)],  # Different possible values for `x_name`.
+        x_vals=[2**i for i in range(0, 24)],  # Different possible values for `x_name`.
         x_log=True,  # x axis is logarithmic.
         line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
         line_vals=['triton', 'torch'],  # Possible values for `line_arg`.
         line_names=['Triton', 'Torch'],  # Label name for the lines.
         styles=[('blue', '-'), ('green', '-')],  # Line styles.
         ylabel='GB/s',  # Label name for the y-axis.
-        plot_name='vector-add-performance',  # Name for the plot. Used also as a file name for saving the plot.
+        plot_name='dot-product-performance',  # Name for the plot. Used also as a file name for saving the plot.
         args={},  # Values for function arguments not in `x_names` and `y_name`.
     ))
 
